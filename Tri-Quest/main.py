e845ba9a1e14ece6bbbf6d197f76fe8697c01340
LeoKratos <61838194+LeoKratos@users.noreply.github.com>
2020-09-10 17:20:07 -0300

    menu com botao funcional

--- Tri-Quest/main.py
+++ Tri-Quest/main.py
@@ -3,10 +3,14 @@ pygame.init()
 clock = pygame.time.Clock()
 menu_inicial = pygame.image.load('imagem\menu.png')
 botao = pygame.image.load('imagem\começar.png')
+x = 0
+y = 150
 
+#imagens
 janela = pygame.display.set_mode((800, 600))
 janela.blit(menu_inicial, (0,0))
 janela.blit(botao, (0,150))
+pygame.display.flip()
 pygame.display.set_caption('Tri-Quest')
 
 janela_aberta = True
@@ -15,6 +19,10 @@ while janela_aberta :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             janela_aberta = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if botao.get_rect().collidepoint(x, y):
+                print('apertou')
 
     pygame.display.update()
 
